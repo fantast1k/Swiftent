695856fcc23f43a1a74c2d88c675bab0611d21b8
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-07-17 23:52:51 +0300

    Add Swiftent Xcode project to implement here the idea

---
+++ Swiftent/Swiftent/Swiftent.h
@@ -0,0 +1,19 @@
+//
+//  Swiftent.h
+//  Swiftent
+//
+//  Created by Dmitry Fa[n]tastik on 17/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiftent.
+FOUNDATION_EXPORT double SwiftentVersionNumber;
+
+//! Project version string for Swiftent.
+FOUNDATION_EXPORT const unsigned char SwiftentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftent/PublicHeader.h>
+
+
