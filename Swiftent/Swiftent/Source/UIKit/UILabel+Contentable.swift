af8d12156eaa1e1251bcfd2576f7b3b3bb359f39
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-07-18 13:17:04 +0300

    Add UILabelContent+Contentable implementation

---
+++ Swiftent/Swiftent/Source/UIKit/UILabel+Contentable.swift
@@ -0,0 +1,25 @@
+//
+//  UILabel+Contentable.swift
+//  Swiftent
+//
+//  Created by Dmitry Fa[n]tastik on 18/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#if os(iOS)
+import UIKit
+
+extension UILabel : Contentable {
+
+    public func installContent(content: UILabelContent) {
+        switch content.text {
+
+        case .Raw(let text):
+            self.text = text
+        case .Attributed(let text):
+            self.attributedText = text
+
+        }
+    }
+}
+#endif
---
+++ Swiftent/Swiftent/Source/UIKit/UILabelContent.swift
@@ -0,0 +1,15 @@
+//
+//  UILabelContent.swift
+//  Swiftent
+//
+//  Created by Dmitry Fa[n]tastik on 18/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+#if os(iOS)
+import UIKit
+
+public struct UILabelContent {
+    let text : Text
+}
+#endif
