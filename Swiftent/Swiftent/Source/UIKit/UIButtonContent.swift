0e941d090091cf1b54d62d2b5401f907c8a15974
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-07-18 19:00:17 +0300

    Improve public initializers for content classes

--- Swiftent/Swiftent/Source/UIKit/UIButtonContent.swift
+++ Swiftent/Swiftent/Source/UIKit/UIButtonContent.swift
@@ -19,17 +19,36 @@ public struct UIButtonContent {
                 highlighted: UIButtonStateContent?,
                 disabled: UIButtonStateContent?,
                 selected: UIButtonStateContent?) {
-        
+
         self.normal = normal
         self.highlighted = highlighted
         self.disabled = disabled
         self.selected = selected
+    }
+
+    public init(normal: UIButtonStateContent?) {
+        self.normal = normal
+        self.highlighted = nil
+        self.disabled = nil
+        self.selected = nil
+    }
+
+    public init(normal: UIButtonStateContent?, selected: UIButtonStateContent?) {
+        self.normal = normal
+        self.selected = selected
+        self.disabled = nil
+        self.highlighted = nil
     }
 }
 
 public struct UIButtonStateContent {
     let text : Text
     let image : UIImage?
+
+    public init(text: Text, image: UIImage?) {
+        self.text = text
+        self.image = image
+    }
 }
 
 #endif
