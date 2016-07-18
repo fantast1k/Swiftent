a3a71dba0179b804021e385876f08cab63272f94
Dmitry Fa[n]tastik <dmitry.fantastik@gmail.com>
2016-07-18 13:16:25 +0300

    Add Common Text enum definition

---
+++ Swiftent/Swiftent/Source/Common/Text.swift
@@ -0,0 +1,14 @@
+//
+//  Text.swift
+//  Swiftent
+//
+//  Created by Dmitry Fa[n]tastik on 18/07/2016.
+//  Copyright © 2016 Fantastik Solution. All rights reserved.
+//
+
+import Foundation
+
+public enum Text {
+    case Raw(String?)
+    case Attributed(NSAttributedString?)
+}
